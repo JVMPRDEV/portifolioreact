9e0b2d888fb0c6fa44919d270ed7059bc1cb4807
Joao Rocha <joao.rocha@easycarros.com>
2025-01-13 15:18:59 -0300

    Ajuste do react-dom

--- src/index.js
+++ src/index.js
@@ -1,9 +1,15 @@
 import React from "react";
-import ReactDOM from "react-dom";
+import { createRoot } from "react-dom/client"; // Importa createRoot do react-dom/client
 import "./index.css"; // Estilos básicos
 import App from "./App";
 
-const root = ReactDOM.createRoot(document.getElementById("root"));
+// Obtém o elemento root do DOM
+const container = document.getElementById("root");
+
+// Cria a instância de root
+const root = createRoot(container);
+
+// Renderiza o aplicativo dentro do React.StrictMode
 root.render(
     <React.StrictMode>
         <App />
